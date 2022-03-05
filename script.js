4442eba8a7a3690b52a203418a42cfdf6745d60c
JhaapKaur <jhaap@hotmail.com>
2022-03-05 21:00:02 +0000

    JS Search Engine Code added
    JS Search Engine Code updated

--- script.js
+++ script.js
@@ -37,3 +37,30 @@ dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+function searchCity(city) {
+  let apiKey = "52ee21443ab65c6b991538e59af12271";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function showWeather(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("h3");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response);
+}
+//call function when someone clicks search
+let city = document.querySelector("#search-form");
+city.addEventListener("submit", handleSubmit);
+
+function retrievePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "52ee21443ab65c6b991538e59af12271";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+navigator.geolocation.getCurrentPosition(retrievePosition);
